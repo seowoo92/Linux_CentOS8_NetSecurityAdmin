94ed4aa299c48fdd81e08f9a3ff9b16e22110dad
seowoo92 <seowoo92@gmail.com>
2023-08-21 15:11:20 +0900

    2nd commit

---
+++ bin/backup2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo >> /var/log/backup.log
+echo >> /var/log/backup.log
+
+echo "===== Backup START : `date` ===========" >> /var/log/backup.log
+
+/root/bin/backup.sh >/dev/null 2>>/var/log/backup.log
+if [ $? -eq 0 ] ; then
+	echo "[ OK ] Backup complete." >> /var/log/backup.log
+else
+	echo "[ FAIL ] Backup failed." >> /var/log/backup.log
+fi
+
+echo "===== Backup STOP : `date` ===========" >> /var/log/backup.log
